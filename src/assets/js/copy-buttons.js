a37922e1dfa4be7ad9328855db39bd9f8d71fe72
Rémino Rem <29999+remino@users.noreply.github.com>
2026-08-19 09:07:09 +0900

    Move Paradimg in Atelier

--- src/assets/js/copy-buttons.js
+++
@@ -1,6 +0,0 @@
-import { addCopyButtons } from '@remino/functions'
-
-addCopyButtons({
-	blockSelector: 'main:not(.playground-layout) .code-block',
-	copiedLabel: 'Copied',
-})
--- src/lib/auto.js
+++
@@ -1,9 +0,0 @@
-import { processImages } from './paradimg.js'
-
-const run = () => processImages()
-
-if (document.readyState === 'loading') {
-	document.addEventListener('DOMContentLoaded', run, { once: true })
-} else {
-	run()
-}
